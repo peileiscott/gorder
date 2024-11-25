f05b067b184f24402083b55c0a2e1b962548f097
Scott Pei <peileiscott@gmail.com>
2024-11-25 15:55:55 +0800

    Create common HTTP & gRPC server

---
+++ internal/common/server/grpc.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"net"
+
+	grpclogrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	grpctags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+)
+
+func init() {
+	logger := logrus.New()
+	logger.SetLevel(logrus.WarnLevel)
+	grpclogrus.ReplaceGrpcLogger(logrus.NewEntry(logger))
+}
+
+func RunGRPCServer(serviceName string, registerServer func(server *grpc.Server)) {
+	addr := viper.Sub(serviceName).GetString("grpc-addr")
+	if addr == "" {
+		logrus.Panicf("please provide grpc-addr for %s service in internal/common/config/global.yaml", serviceName)
+	}
+	RunGRPCServerOnAddr(addr, registerServer)
+}
+
+func RunGRPCServerOnAddr(addr string, registerServer func(server *grpc.Server)) {
+	logrusEntry := logrus.NewEntry(logrus.StandardLogger())
+	grpcServer := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			grpctags.UnaryServerInterceptor(grpctags.WithFieldExtractor(grpctags.CodeGenRequestFieldExtractor)),
+			grpclogrus.UnaryServerInterceptor(logrusEntry),
+		),
+		grpc.ChainStreamInterceptor(
+			grpctags.StreamServerInterceptor(grpctags.WithFieldExtractor(grpctags.CodeGenRequestFieldExtractor)),
+			grpclogrus.StreamServerInterceptor(logrusEntry),
+		),
+	)
+	registerServer(grpcServer)
+
+	listen, err := net.Listen("tcp", addr)
+	if err != nil {
+		logrus.Panic(err)
+	}
+
+	logrus.Infof("grpc server is running on %s", addr)
+	if err := grpcServer.Serve(listen); err != nil {
+		logrus.Panic(err)
+	}
+}
---
+++ internal/common/server/http.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func RunHTTPServer(serviceName string, wrapper func(router *gin.Engine)) {
+	addr := viper.Sub(serviceName).GetString("http-addr")
+	if addr == "" {
+		logrus.Panicf("please provide http-addr for %s service in internal/common/config/global.yaml", serviceName)
+	}
+	RunHTTPServerOnAddr(addr, wrapper)
+}
+
+func RunHTTPServerOnAddr(addr string, wrapper func(router *gin.Engine)) {
+	apiRouter := gin.New()
+	wrapper(apiRouter)
+	apiRouter.Group("/api")
+	if err := apiRouter.Run(addr); err != nil {
+		logrus.Panic(err)
+	}
+}
