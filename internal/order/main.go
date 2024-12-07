a33c542922dafeb3077892f2390cd05f8327b1d7
Scott Pei <peileiscott@gmail.com>
2024-12-07 21:47:33 +0800

    Feat: Inject app into HTTP & gRPC server

--- internal/order/main.go
+++ internal/order/main.go
@@ -1,30 +1,39 @@
 package main
 
 import (
+	"context"
+
 	"github.com/gin-gonic/gin"
 	"github.com/peileiscott/gorder/common/config"
 	"github.com/peileiscott/gorder/common/genproto/orderpb"
 	"github.com/peileiscott/gorder/common/server"
 	"github.com/peileiscott/gorder/order/ports"
+	"github.com/peileiscott/gorder/order/service"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
 
 func init() {
 	if err := config.NewViperConfig(); err != nil {
-		panic(err)
+		logrus.Panic(err)
 	}
 }
 
 func main() {
 	serviceName := viper.GetString("order.service_name")
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	application := service.NewApplication(ctx)
+
 	go server.RunGRPCServer(serviceName, func(s *grpc.Server) {
-		orderpb.RegisterOrderServiceServer(s, ports.NewGRPCServer())
+		orderpb.RegisterOrderServiceServer(s, ports.NewGRPCServer(application))
 	})
 
 	server.RunHTTPServer(serviceName, func(router *gin.Engine) {
-		ports.RegisterHandlersWithOptions(router, ports.NewHTTPServer(), ports.GinServerOptions{
+		ports.RegisterHandlersWithOptions(router, ports.NewHTTPServer(application), ports.GinServerOptions{
 			BaseURL:      "/api",
 			Middlewares:  nil,
 			ErrorHandler: nil,
--- internal/order/ports/http.go
+++ internal/order/ports/http.go
@@ -1,11 +1,16 @@
 package ports
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/peileiscott/gorder/order/app"
+)
 
-type HTTPServer struct{}
+type HTTPServer struct {
+	app app.Application
+}
 
-func NewHTTPServer() *HTTPServer {
-	return &HTTPServer{}
+func NewHTTPServer(app app.Application) *HTTPServer {
+	return &HTTPServer{app: app}
 }
 
 func (s HTTPServer) CreateOrder(c *gin.Context, customerID string) {
